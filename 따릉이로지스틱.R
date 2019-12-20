004ac592235f557c880711373a294ab809831728
jaewon-jun9 <jjw0525@naver.com>
2019-12-20 18:11:18 +0900

    project

--- 따릉이로지스틱.R
+++ 따릉이로지스틱.R
@@ -1,8 +1,9 @@
 library(stats)
 bikeL<- read.csv(file.choose(), header=T ,stringsAsFactors = F) #csv
-head(bikeL)
+head(bikeL,50)
 name=c("bike_id","start_stn","end_stn","eq","use_time_o","b_year","b_month","b_day","b_hour","b_min","b_weekday","r_year","r_month","r_day","r_hour","r_min","r_weekday","use_time_m")
 names(bikeL)=name
+str(bikeL)
 summary(bikeL)
 
 par(mfrow = c(2, 2))
@@ -24,33 +25,37 @@ bikeL$r_weekday=as.factor(bikeL$r_weekday)
 
 summary(bikeL)
 
-bikeLsub=bikeL[(bikeL$use_time_o < 44&bikeL$use_time_o > 0&bikeL$eq == 0|bikeL$use_time_o < 135&bikeL$use_time_o > 0&bikeL$eq == 1)&bikeL$b_year==2016,]
+bikeLsub=bikeL[(bikeL$use_time_o < 44&bikeL$use_time_o > 0&bikeL$eq == "0"|bikeL$use_time_o < 135&bikeL$use_time_o > 0&bikeL$eq == "1")&bikeL$b_year=="2019"&bikeL$start_stn=="207",]
 summary(bikeLsub)
 
 bikelogit<-glm(eq ~ b_month+b_hour+b_weekday , data = bikeLsub, family = 'binomial') #b_year+b_month+
 summary(bikelogit)
 bikelogit
 a=bikeLsub$eq
-b=ifelse(fitted(bikelogit)>0.1,1,0)
+b=ifelse(fitted(bikelogit)>0.5,1,0)
+q=table(a[1:266]==b[1:266])
+q[2]/(q[1]+q[2])#맞춘 비율
+table(bikeLsub$eq)[2]/(table(bikeLsub$eq)[1]+table(bikeLsub$eq)[2])#투입 비율
+
+plot(bikelogit)
+
 #table(a==b)
-q=table(a==b)
-q[2]/(q[1]+q[2])
 
-table(bikeLsub$eq)[1]/(table(bikeLsub$eq)[1]+table(bikeLsub$eq)[2])
+head(bikeLsub)
 
+g=fitted(bikelogit)>0.5
+h=predict(bikelogit, bikeLsub, type = "response")>0.5
+g==h
 
+table(bikeLsub$eq,predict(bikelogit, bikeLsub, type = "response")>0.5)
 
-
-
-
-
-
-
+rgr<- lm(use_time_m~b_month+b_hour+b_weekday, data=bikeLsub)
+summary(rgr)
 
 
-
-
-
+head(bikeLsub,100)
+fitted(rgr)[1:100]
+predict(m,newdata = data.frame(speed=200))
 
 
 
@@ -61,7 +66,16 @@ table(bikeLsub$eq)[1]/(table(bikeLsub$eq)[1]+table(bikeLsub$eq)[2])
 
 
 
-bikeL207=bikeL[bikeL$start_stn == "207"&bikeL$use_time_o < 44&bikeL$use_time_o > 0&bikeL$eq == 0|bikeL$start_stn == "207"&bikeL$use_time_o < 135&bikeL$use_time_o > 0&bikeL$eq == 1,]
+
+
+
+
+
+
+
+
+
+bikeL207=bikeL[bikeL$start_stn == "207"&bikeL$use_time_o < 44&bikeL$use_time_o > 0&bikeL$eq == "0"|bikeL$start_stn == "207"&bikeL$use_time_o < 135&bikeL$use_time_o > 0&bikeL$eq == "1",]
 summary(bikeL207)
 
 bikelogit<-glm(eq ~ b_month+b_hour+b_weekday , data = bikeL207, family = 'binomial') #b_year+b_month+
@@ -69,7 +83,6 @@ summary(bikelogit)
 bikelogit
 a=bikeL207$eq
 b=ifelse(fitted(bikelogit)>0.5,1,0)
-#table(a==b)
 q=table(a==b)
 q[2]/(q[1]+q[2])
 
